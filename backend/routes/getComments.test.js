1666a976a776045b2566660484c9bdbb8b476208
Simon Backx <hi@simonbackx.com>
2022-01-28 15:11:03 +0100

    Added test to check if we only return current user's votes

--- backend/routes/getComments.test.js
+++ backend/routes/getComments.test.js
@@ -4,13 +4,16 @@ const User = require('../models/User');
 const Comment = require('../models/Comment');
 const Vote = require('../models/Vote');
 
-let user;
+let user, otherUser;
 let comment;
 
 describe("Get comments", () => {
     beforeAll(async () => {
         user = new User({ name: "Test user" })
         await user.save()
+
+        otherUser = new User({ name: "Other test user" })
+        await otherUser.save()
     })
 
     // Since we clear the database on every test file run, we know that comments should be empty
@@ -22,20 +25,11 @@ describe("Get comments", () => {
         expect(response.body.votes).toEqual([]);
     });
 
-    test("get comments, unauthenticated", async () => {
+    test("get comments, authenticated, without votes", async () => {
         // Create a comment
         comment = new Comment({ message: "Hello world", user_id: user.id })
         await comment.save()
 
-        // Check if we get this comment
-        const response = await request(app).get('/comments').send()
-        
-        expect(response.status).toEqual(200);
-        expect(response.body.comments).toEqual([comment.toJSON()]);
-        expect(response.body.votes).toEqual([]);
-    });
-
-    test("get comments, authenticated, without votes", async () => {
         // Check if we get this comment
         const response = await request(app).get('/comments').set("Authorization", "USER_ID "+user.id).send()
         
@@ -45,9 +39,12 @@ describe("Get comments", () => {
     });
 
     test("get comments, authenticated, with votes", async () => {
-        // Create vote
+        // Create votes
         vote = new Vote({ comment_id: comment.id, user_id: user.id })
         await vote.save()
+
+        const otherVote = new Vote({ comment_id: comment.id, user_id: otherUser.id })
+        await otherVote.save()
 
         // Check if we get this comment
         const response = await request(app).get('/comments').set("Authorization", "USER_ID "+user.id).send()
@@ -55,5 +52,16 @@ describe("Get comments", () => {
         expect(response.status).toEqual(200);
         expect(response.body.comments).toEqual([comment.toJSON()]);
         expect(response.body.votes).toEqual([vote.toJSON()]);
+    });
+
+    test("get comments, unauthenticated", async () => {
+        // Votes should stay empty
+
+        // Check if we get this comment
+        const response = await request(app).get('/comments').send()
+        
+        expect(response.status).toEqual(200);
+        expect(response.body.comments).toEqual([comment.toJSON()]);
+        expect(response.body.votes).toEqual([]);
     });
 })
