274ce03fe37e07d5ff9dea651abdc76ce557be47
Simon Backx <hi@simonbackx.com>
2022-01-28 14:27:11 +0100

    Added extra documentation for the Auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,14 @@
 const User = require("../models/User")
 const ClientError = require("../errors/ClientError")
 
-// Authentication is purely based on the user_id for now (not secure). 
-// The signIn route is used to get a user id and store it client side
-
+/**
+ * Authentication is purely based on the user_id for now (not secure). 
+ * The signIn route is used to get a user id and store it client side
+ * 
+ * You can make authentication required or optional by setting the required property of the options object
+ * @param {Object} options
+ * @param {Object} options.required
+ */
 module.exports = function({ required } = { required: true }) {
     return async function (req, res, next) {
         if (!req.headers["authorization"] && !required) {
