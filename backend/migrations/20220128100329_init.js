fad3b7e89cd8499396e8d03562f930eee57e9674
Simon Backx <hi@simonbackx.com>
2022-01-29 09:41:43 +0100

    Added emoji support

--- backend/migrations/20220128100329_init.js
+++ backend/migrations/20220128100329_init.js
@@ -4,6 +4,9 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable('users', function(table) {
+        table.charset('utf8mb4');
+        table.collate('utf8mb4_0900_ai_ci');
+        
         table.increments('id').primary();
         table.string('name').notNullable();
     });
--- backend/models/bookshelf/databaseConfig.js
+++ backend/models/bookshelf/databaseConfig.js
@@ -15,7 +15,7 @@ if (process.env.environment === "test") {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
-            charset: 'utf8'
+            charset: 'utf8mb4'
         }
     }
 }
