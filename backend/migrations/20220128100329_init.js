bcca97d04757cccd8066300d6e5457143d0d7836
Simon Backx <hi@simonbackx.com>
2022-01-29 10:08:09 +0100

    Fix: charset in SQLite not supported

--- backend/migrations/20220128100329_init.js
+++ backend/migrations/20220128100329_init.js
@@ -4,8 +4,11 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable('users', function(table) {
-        table.charset('utf8mb4');
-        table.collate('utf8mb4_0900_ai_ci');
+        if (process.env.environment !== "test") {
+            // Charset not supported in SQLite
+            table.charset('utf8mb4');
+            table.collate('utf8mb4_0900_ai_ci');
+        }
         
         table.increments('id').primary();
         table.string('name').notNullable();
