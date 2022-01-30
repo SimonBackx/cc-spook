83259c1170ba12c2acf0c6cf5e1ec637fb4bfac6
Simon Backx <hi@simonbackx.com>
2022-01-30 13:46:12 +0100

    Added support for DB_PORT env variable

--- backend/models/bookshelf/databaseConfig.js
+++ backend/models/bookshelf/databaseConfig.js
@@ -15,6 +15,7 @@ if (process.env.environment === "test") {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT ?? 3306,
             charset: 'utf8mb4'
         }
     }
